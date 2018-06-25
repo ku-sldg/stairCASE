205e7323e42581bd38fc17ca624607efceb3488f
Michael Neises <neisesmichael@gmail.com>
2018-06-25 16:54:26 -0500

    run to end, admitted proofs

--- examples/compcert/experiment1/exp1.c
+++ examples/compcert/experiment1/exp1.c
@@ -2,16 +2,16 @@
 // 22/6/18
 //tiny program for use in the VST
 
-const int mySum(int x, int y)
+int mySum(int x, int y)
 {
-    const int z = x + y;
+    int z = x + y;
     return z;
 }
 
 int main(void)
 {
-    const int A = 2;
-    const int B = 3;
-    const int C = mySum( A, B );
+    int A = 2;
+    int B = 3;
+    int C = mySum( A, B );
     return C;
 }
