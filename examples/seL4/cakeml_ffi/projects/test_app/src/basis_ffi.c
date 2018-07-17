8088286570366b162e85d670e7d0ba5f6bfb19a4
Grant Jurgensen <gajurgensen@gmail.com>
2018-07-17 12:48:43 -0500

    example of cakeml ffi return

--- examples/seL4/cakeml_ffi/projects/test_app/src/basis_ffi.c
+++ examples/seL4/cakeml_ffi/projects/test_app/src/basis_ffi.c
@@ -8,15 +8,33 @@
 #include <assert.h>
 #include <stdio.h>
 
+// Internal helper function, not exposed to cakeml. Used by ffifoo.
+// Copies string `from` into `to`, checking against length of `to`.
+// Assumes `from` is null-terminated (string literals are by default).
+// `to` will always be null-terminated.
+void str_cpy(unsigned char *from, unsigned char *to, long toLen){
+  for (long i = 0; i < (toLen - 1); i++) {
+    to[i] = from[i];
+    if (to[i] == '\0')
+      return;
+  }
+  to[toLen -1] = '\0';
+}
+
 // Test ffi function. The parameters appear to be mandatory.
-// While we are given the length for our char arrays, they also appear to be
-// null terminated, judging by the success of printf.
+// The strings are null terminated. The length parameters refer to the length
+// of the underlying allocated array.
 // Also, while c and a have the same type here, they seem to be treated
 // differently in cakeml. c is an actual string, where a is a word8array?
 void ffifoo(unsigned char *c, long clen, unsigned char *a, long alen) {
+  // print "foo" + input string a
   printf("foo %s\n", c);
+
+  // copy "bar" into return string a
+  str_cpy("bar", a, alen);
 }
 
+// CakeML returns here rather than back into main
 void cml_exit(int arg) {
   printf("Cakeml finished, now in cml_exit subroutine in basis_ffi.c\n");
   exit(arg);
